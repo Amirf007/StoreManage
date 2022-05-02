0d1451df5bffc0b24d4a285439e867b805b506e3
Amir <amirfeyzipoor0007@gmail.com>
2022-05-01 21:41:23 -0700

    Create StoreManage.Entitiese layer

---
+++ src/StoreManage.Entities/BuyFactor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreManage.Entities
+{
+    public class BuyFactor
+    {
+        public int BuyFactorNumber { get; set; }
+        public DateTime Date { get; set; }
+        public string BuyPrice { get; set; }
+        public string Count { get; set; }
+        public string SellerName { get; set; }
+        public Commodity Commodity { get; set; }
+        public int CommodityId { get; set; }
+    }
+}
