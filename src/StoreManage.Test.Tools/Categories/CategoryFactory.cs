69f14680e51f3ec2bf0ff4ccb59e3d51cfa85509
Amir <amirfeyzipoor0007@gmail.com>
2022-05-03 03:53:51 -0700

    Create StoreManage.Test.Tools in testsfolder for create category and commodity for tests

---
+++ src/StoreManage.Test.Tools/Categories/CategoryFactory.cs
@@ -0,0 +1,20 @@
+﻿using StoreManage.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreManage.Test.Tools.Categories
+{
+    public static class CategoryFactory
+    {
+        public static Category CreateCategory()
+        {
+            return new Category
+            {
+                Title = "لبنیات"
+            };
+        }
+    }
+}
---
+++ src/StoreManage.Test.Tools/Commodities/CommodityFactory.cs
@@ -0,0 +1,25 @@
+﻿using StoreManage.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreManage.Test.Tools.Commodities
+{
+    public static class CommodityFactory
+    {
+        public static Commodity CreateCommodity(int categoryId)
+        {
+            return new Commodity
+            {
+                Name = "ماست رامک",
+                Price = "150000",
+                Inventory = 10,
+                MaxInventory = "15",
+                MinInventory = "5",
+                CategoryId = categoryId,
+            };
+        }
+    }
+}
