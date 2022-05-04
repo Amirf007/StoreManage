0d63f7dd2a772021986d13b6a1395f7fc3f54e25
Amir <amirfeyzipoor0007@gmail.com>
2022-05-04 13:00:24 -0700

    Complete Categorycontroller for restapi

--- src/StoreManage.RestAPI/Controllers/CategoryController.cs
+++ src/StoreManage.RestAPI/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StoreManage.Services.Categories.Contracts;
+using System.Collections.Generic;
 
 namespace StoreManage.RestAPI.Controllers
 {
@@ -6,5 +8,40 @@ namespace StoreManage.RestAPI.Controllers
     [ApiController]
     public class CategoryController : ControllerBase
     {
+        private readonly CategoryService _service;
+        public CategoryController(CategoryService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public void Add(AddCategoryDto dto)
+        {
+            _service.Add(dto);
+        }
+
+        [HttpGet("{id}")]
+        public GetCategoryDto GetCategory(int id)
+        {
+            return _service.GetCategory(id);
+        }
+
+        [HttpGet]
+        public IList<GetCategoryDto> Getall()
+        {
+            return _service.GetAll();
+        }
+
+        [HttpPut("{id}")]
+        public void Update(int id, UpdateCategoryDto dto)
+        {
+            _service.Update(id, dto);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _service.Delete(id);
+        }
     }
 }
